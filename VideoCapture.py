3c54a4bc1cec8b2edd8bd0e054331d31287c253f
Jose Pablo Marin Valera <elfindetusdias@gmail.com>
2018-10-19 21:38:09 +0200

    video capture & keypoint

--- VideoCapture.py
+++ VideoCapture.py
@@ -8,27 +8,32 @@ H=1080//2
 
 pygame.init()
 screen=pygame.display.set_mode((W,H))
-
+orb = cv2.ORB_create()
 
 def process_frame(img):
+    kp, des = orb.detectAndCompute(img,None)
+    for p in kp:
+        print(p)
+
     img=cv2.resize(img,(W,H))
     cv2.imshow("image",img)
-    """cv2.waitKey()
-    print(img.shape)"""
-    
+"""cv2.waitKey()
+print(img.shape)"""
+
 
 if __name__=="__main__":
     cap=cv2.VideoCapture("./videos/test.mp4")
 
-    while cap.isOpened():
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        if ret == True:
-            process_frame(frame)
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-        else:
+while cap.isOpened():
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret == True:
+        process_frame(frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
             break
-        
+    else:
+        break
+    
 cap.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()        
+
